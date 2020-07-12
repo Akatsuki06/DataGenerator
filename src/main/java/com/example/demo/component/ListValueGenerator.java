505aa8aafe8cc8e3053b4cc8daccd22c1c6a7890
Akatsuki06 <suraznegi@gmail.com>
2020-07-12 15:26:36 +0530

    checkpoint for literal completed

--- src/main/java/com/example/demo/component/ListValueGenerator.java
+++ src/main/java/com/example/demo/component/ListValueGenerator.java
@@ -33,30 +33,40 @@ public class ListValueGenerator {
     public void generate(Map<String,Object> output, Map<String,Object> props, String key) throws Exception {
 
         List<Object> outputList = new ArrayList<>();
-        int max_len =(int) props.get(DataDefinition.MAX_LEN.toString());//its mandatory
-        int min_len = (int) props.getOrDefault(DataDefinition.MIN_LEN.toString(),0);
-        int nObj = DataUtility.generateRandomIntInRange(min_len,max_len+1);
-        Map<String,Object> index = (Map<String, Object>) props.get("index");
-        String indexType = ((String)index.get(DataDefinition.TYPE.toString()));
-        Map<String,Object> mapData = new HashMap<>();
-        for (int i = 0; i <nObj ; i++) {
-            if (ObjectType.OBJECT.equalsIgnoreCase(indexType)){
-                    objectDataGenerator.generate(mapData,(Map<String, Object>) index.get(DataDefinition.DATA.toString()),key);
-            }else if (ObjectType.LITERAL.equalsIgnoreCase(indexType)){
-                   literalValueGenerator.generate(mapData,index,key);
-            }else if(ObjectType.LIST.equalsIgnoreCase(indexType)){
-                    this.generate(mapData,index,key);
-            }else{
-                System.out.println("Not valid Type");
-                throw new UndefinedTypeException("The type "+indexType+" is not a defined type.");
+        String id = (String)props.get("use");
+        String checkPoint = (String) props.get("checkpoint");
+
+
+        if (id!=null){
+            checkpointResolver.use(id,key,output);
+        }else{
+            //optiaonl conditino here
+            // if optional then with some probability outputList can be set to Null
+            int max_len =(int) props.get(DataDefinition.MAX_LEN.toString());//its mandatory
+            int min_len = (int) props.getOrDefault(DataDefinition.MIN_LEN.toString(),0);
+            int nObj = DataUtility.generateRandomIntInRange(min_len,max_len+1);
+            Map<String,Object> index = (Map<String, Object>) props.get("index");
+            String indexType = ((String)index.get(DataDefinition.TYPE.toString()));
+            Map<String,Object> mapData = new HashMap<>();
+            for (int i = 0; i <nObj ; i++) {
+                if (ObjectType.OBJECT.equalsIgnoreCase(indexType)){
+                        objectDataGenerator.generate(mapData,(Map<String, Object>) index.get(DataDefinition.DATA.toString()),key);
+                }else if (ObjectType.LITERAL.equalsIgnoreCase(indexType)){
+                       literalValueGenerator.generate(mapData,index,key);
+                }else if(ObjectType.LIST.equalsIgnoreCase(indexType)){
+                        this.generate(mapData,index,key);
+                }else{
+                    System.out.println("Not valid Type");
+                    throw new UndefinedTypeException("The type "+indexType+" is not a defined type.");
+                }
+                outputList.add(mapData.get(key));
             }
-            outputList.add(mapData.get(key));
-        }
-        if (props.get(CHECKPOINT)!=null){
-            System.out.println("saving...");
-            checkpointResolver.checkpoint(String.valueOf(props.get(CHECKPOINT)),outputList);
         }
         output.put(key,outputList);
+
+        if (id==null && checkPoint!=null){
+            checkpointResolver.save(checkPoint,outputList);
+        }
     }
 
 
--- src/main/java/com/example/demo/component/ObjectDataGenerator.java
+++ src/main/java/com/example/demo/component/ObjectDataGenerator.java
@@ -32,31 +32,37 @@ public class ObjectDataGenerator  {
     public void generate(Map<String,Object> output,Map<String,Object> objProps, String key) throws Exception{
         Map<String,Object> mapData = new HashMap<>();
 
+        String id = (String)objProps.get("use");
+        String checkPoint = (String) objProps.get("checkpoint");
 
-        //check for USE first if its USE go ahead and write to table
-
-
-        Map<String,Object> keys = (Map<String,Object>)objProps.get("keys");
-        for (Map.Entry<String,Object> obj : keys.entrySet()){
+        if (id!=null){
+            checkpointResolver.use(id,key,output);
+        }else {
+            Map<String, Object> keys = (Map<String, Object>) objProps.get("keys");
+            for (Map.Entry<String, Object> obj : keys.entrySet()) {
                 String objKey = obj.getKey();
                 Map<String, Object> props = (Map<String, Object>) obj.getValue();
                 String type = String.valueOf(props.get(DataDefinition.TYPE.toString()));
                 String optional = String.valueOf(props.get(DataDefinition.OPTIONAL.toString()));
 
                 if (ObjectType.LITERAL.equalsIgnoreCase(type)) {
-                     literalValueGenerator.generate(mapData,props, objKey);
+                    literalValueGenerator.generate(mapData, props, objKey);
                 } else if (ObjectType.LIST.equalsIgnoreCase(type)) {
-                    listValueGenerator.generate(mapData,props, objKey);
+                    listValueGenerator.generate(mapData, props, objKey);
                 } else if ((ObjectType.OBJECT.equalsIgnoreCase(type))) {
-                    generate(mapData,(Map<String, Object>) props.get(DataDefinition.DATA.toString()),objKey);
+                    generate(mapData, (Map<String, Object>) props.get(DataDefinition.DATA.toString()), objKey);
                 } else {
-                    throw new UndefinedTypeException("The type `"+type+"` is not a defined type.");
+                    throw new UndefinedTypeException("The type `" + type + "` is not a defined type.");
                 }
+            }
         }
 
 
         output.put(key,mapData);
 
+        if (checkPoint!=null){
+            checkpointResolver.save(checkPoint,mapData);
+        }
         //Check for CHECKPOINT at the end, go ahead and save it to table
 
     }
